dcb1476f9a124387be5e61caf1443a147f18d77f
DiarRantiz <halorohman123@gmail.com>
2024-11-18 22:33:24 +0700

    first commit

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'SPD-01',
+      slug: 'BMX',
+      name: "Sepeda BMX",
+      category: 'Best Seller',
+      price: 2_000_000,
+      stock: 10,
+      imageUrl: '/assets/images/BMX.jpg',
+      description: `Sepeda BMX merupakan pilihan yang tepat bagi anda yang sedang mencari sepeda anak. BMX adalah sepeda yang dirancang untuk pemakaian anak yang aktif bermain dan sedang tumbuh.`,
+ 
+    },
+    {
+      id: 'SPD-02',
+      slug: 'Hybrid',
+      name: "Sepeda Hybrid",
+      category: 'Popular',
+      price: 1_500_000,
+      stock: 70,
+      imageUrl: '/assets/images/Hybrid.jpg',
+      description: `Sepeda Hybrid kini merupakan jenis sepeda banyak diminati oleh berbagai kalangan. Sepeda ini didesain secara simpel dan memiliki roda gear yang lebih sedikit, dan cocok untuk digunakan bersepeda santai. Sepeda Hybrid cocok bagi Anda yang tinggal di area kota yang padat dan jalanan yang datar, karena tidak dibekali banyak roda gear. Anda dapat memakainya untuk jalan-jalan, berolahraga, ataupun menggunakannya sebagai alat transportasi ke kantor atau sekolah.`,
+     
+    },
+    {
+      id: 'SPD-03',
+      slug: 'Lipat',
+      name: "Sepeda Lipat",
+      category: 'Popular',
+      price: 1_000_000,
+      stock: 85,
+      imageUrl: '/assets/images/Lipat.jpg',
+      description: `Sepeda lipat didesain secara simpel dan kelebihannya yang bisa dilipat sehingga mudah dibawa ke manapun membuatnya menjadi salah satu pilihan populer terutama di area yang padat penduduk. Tak cuma minim tempat di luar, saat di simpan di rumah pun, anda bisa menghemat ruang dengan melipatnya.`,
+     
+    },
+    {
+      id: 'SPD-04',
+      slug: 'Fixie',
+      name: "Sepeda Fixie",
+      category: 'Best Seller',
+      price: 3_000_000,
+      stock: 25,
+      imageUrl: '/assets/images/Fixie.jpg',
+      description: `Sepeda Fixie adalah sepeda dulu pernah populer, sepeda ini masih tidak jarang di jumpai. Meskipun, sepeda ini kalah popularitasnya dibanding tipe sepeda lain. Salah satu faktornya adalah karena sepeda Fixie tidak dibekali rem, sehingga pengguna dituntut keahlian khusus agar dapat menungganginya. Namun bagi beberapa pengguna yang suka dengan desainnya yang klasik, ada yang memodifikasi sendiri agar mempunyai rem.`,
+ 
+    },
+    {
+      id: 'SPD-05',
+      slug: 'Touring',
+      name: "Sepeda Touring",
+      category: 'Popular',
+      price: 2_000_000,
+      stock: 21,
+      imageUrl: '/assets/images/Touring.jpg',
+      description: `Seperti namanya, sepeda touring adalah tipe sepeda yang digunakan perjalanan jauh, melakukan touring dengan menggunakan sepeda. Sepeda jenis ini menyajikan kenyaman saat menggunakannya dan tidak membutuhkan tenaga yang besar saat mengayuhnya, sehingga anda dapat menempuh jarak yang lebih jauh lebih enteng dibanding sepeda jenis lainnya.`,
+ 
+    },
+    {
+      id: 'SPD-06',
+      slug: 'Listrik',
+      name: "Sepeda Listrik",
+      category: 'Popular',
+      price: 5_000_000,
+      stock: 99,
+      imageUrl: '/assets/images/Listrik.jpg',
+      description: `Sepeda listrik menyajikan kepraktisan bagi penggunanya, dimana Anda dapat memilih ingin menggerakknya dengan tenaga sendiri ataupun tenaga listrik. Sepeda jenis ini cocok untuk bagi pengguna yang tinggal di perkotaan, mengingat bahwa sepeda ini didesain agar cocok dengan jalanan kota yang rata. Sebaliknya, sepeda ini tidak disarankan bagi Anda yang berada di pedesaan dengan jalan yang tidak rata, bergelombang, atau banyak tanjakan.`,
+
+    },
+    {
+      id: 'SPD-07',
+      slug: 'MTB',
+      name: "Sepeda Gunung (MTB)",
+      category: 'Best Seller',
+      price: 6_000_000,
+      stock: 35,
+      imageUrl: '/assets/images/MTB.jpg',
+      description: `Sepeda Gunung (MTB) tersedia hampir untuk seluruh usia mulai anak-anak, remaja, hingga orang dewasa. Meski dinamakan sepeda gunung, namun pemakaiannya tidak tertuju hanya di area pegunungan. Anda dapat menggunakannya offroad, berolahraga, jalan-jalan, baik di jalan datar, menanjak, jalan bergelombak, maupun area-area tidak stabil lainnya. Selain itu sepeda jenis ini bagus untuk kesehatan, karena didesain untuk pemakaian di area pegunungan, kebanyakan pengguna menggunakannya untuk berolahraga dan dapat memabntu pertumbuhan tinggi badan pengguna.`,
+ 
+    },
+    {
+      id: 'SPD-08',
+      slug: 'Balap',
+      name: "Sepeda Balap (Road Bike)",
+      category: 'Best Seller',
+      price: 7_000_000,
+      stock: 40,
+      imageUrl: '/assets/images/Balap.jpg',
+      description: `Bagi anda yang hobi mengikuti event dan berkompetisi, sepeda balap (Road Bike) merupakan pilihan terbaik. Sepeda balap dirancang lebih ringan dan sporty, sehingga anda dapat menghasilkan kecepatan tinggi meski mengayuh dengan ringan. Walaupun disebut sepeda balap, sepeda ini tidak tertutup hanya untuk kompetisi. Karena dirancang untuk berbagai medan, anda dapat menggunakannya untuk berbagai kebutuhan misalnya bersepeda santai di taman, berangkat ke kantor di jalan raya, berolahraga di pegunungan dan lain-lain.`,
+ 
+    },
+
+  ]
+}
